52a138d5f077e2e59fc25bcd84f1e80a80cc5cb5
Ian Grant <iagrant14@gmail.com>
2018-02-02 14:21:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo Exporting packages
-echo init.vim coppied over
-mv .config/nvim/* ~/.config/nvim
+echo rcs copied over
+mv .vimrc ~/.vimrc
 echo .bashrc
 mv .bashrc ~/.bashrc
 echo .tmux.conf
@@ -14,10 +14,10 @@ mv .i3/* ~/.i3/
 echo polybar
 mv .config/polybar/* ~/.config/polybar/
 echo Making Vim Plugin Directory
-mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle && \
-curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo Vim Airline
-cd ~/.config/nvim/bundle/
+cd ~/.vim/bundle/
 git clone https://github.com/vim-airline/vim-airline.git
 echo Vim Airline Themes
 git clone https://github.com/vim-airline/vim-airline-themes.git
@@ -25,27 +25,27 @@ echo Vim Color-schemes
 git clone https://github.com/flazz/vim-colorschemes.git
 echo Vim Vinegar
 git clone https://github.com/tpope/vim-vinegar.git
-echo Tmuxline
-git clone https://github.com/edkolev/tmuxline.vim.git
+#echo Tmuxline
+#git clone https://github.com/edkolev/tmuxline.vim.git
 echo Vim Bufferline
 git clone https://github.com/bling/vim-bufferline.git
-echo NeoMake
-git clone https://github.com/neomake/neomake.git
-echo Neco-Ghc
-git clone https://github.com/eagletmt/neco-ghc
-echo Haskell-Vim
-https://github.com/neovimhaskell/haskell-vim.git
-echo Ghc-Mod
-git clone https://github.com/eagletmt/ghcmod-vim.git
+#Uncomment Later when i decided which ones i actually use and need
+#and possible figure out which one makes it feel slow
+#echo Neco-Ghc
+#git clone https://github.com/eagletmt/neco-ghc
+#echo Haskell-Vim
+#https://github.com/neovimhaskell/haskell-vim.git
+#echo Ghc-Mod
+#git clone https://github.com/eagletmt/ghcmod-vim.git
 echo Vim-Fugitive
 git clone https://github.com/tpope/vim-fugitive.git
 echo Vim-Rhubarb
 git clone https://github.com/tpope/vim-rhubarb.git
-echo Vim-Proc # Needed by Ghc-mod
-git clone https://github.com/Shougo/vimproc.vim.git
-cd vimproc.vim
-make
-cd ..
+#echo Vim-Proc # Needed by Ghc-mod
+#git clone https://github.com/Shougo/vimproc.vim.git
+#cd vimproc.vim
+#make
+#cd ..
 echo YouCompleteMe
 git clone https://github.com/Valloric/YouCompleteMe.git
 cd YouCompleteMe
