9134619c6a6bedfd33f7b4df1de807b8f69458a5
Ian-Grant <iagrant14@gmail.com>
2018-05-20 14:24:25 -0700

    Sepreated Installers

---
+++ vimInstall.sh
@@ -0,0 +1,37 @@
+echo Making Vim Plugin Directory
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo Vim Airline
+cd ~/.vim/bundle/
+git clone https://github.com/vim-airline/vim-airline.git
+echo Vim Airline Themes
+git clone https://github.com/vim-airline/vim-airline-themes.git
+echo Vim Color-schemes
+git clone https://github.com/flazz/vim-colorschemes.git
+echo Vim Vinegar
+git clone https://github.com/tpope/vim-vinegar.git
+#echo Tmuxline
+#git clone https://github.com/edkolev/tmuxline.vim.git
+echo Vim Bufferline
+git clone https://github.com/bling/vim-bufferline.git
+#Uncomment Later when i decided which ones i actually use and need
+#and possible figure out which one makes it feel slow
+#echo Neco-Ghc
+#git clone https://github.com/eagletmt/neco-ghc
+#echo Haskell-Vim
+#https://github.com/neovimhaskell/haskell-vim.git
+#echo Ghc-Mod
+#git clone https://github.com/eagletmt/ghcmod-vim.git
+echo Vim-Fugitive
+git clone https://github.com/tpope/vim-fugitive.git
+echo Vim-Rhubarb
+git clone https://github.com/tpope/vim-rhubarb.git
+#echo Vim-Proc # Needed by Ghc-mod
+#git clone https://github.com/Shougo/vimproc.vim.git
+#cd vimproc.vim
+#make
+#cd ..
+echo YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.py
