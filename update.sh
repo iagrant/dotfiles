1cb07e7525a148770cde8b9052ee08e06d6bed9e
Ian Grant <igrant@nevada.unr.edu>
2018-10-15 23:06:12 -0700

    update rcs and structure

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #copy rcs
-cp ~/.vimrc rcs/.vimrc
-cp ~/.zshrc rcs/.zshrc
-cp ~/.bashrc rcs/.bashrc
-cp ~/.config/i3/config .config/i3/config
-cp ~/.config/nvim/init.vim .config/nvim/init.vim
+cp ~/.vimrc rcs/vimrc
+cp ~/.zshrc rcs/zshrc
+cp ~/.bashrc rcs/bashrc
